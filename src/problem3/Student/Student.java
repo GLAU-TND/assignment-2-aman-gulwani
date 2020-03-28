fb5e525985f6357b643b0292b6542820cc035076
aman-gulwani <aman.gulwani_cs18@gla.ac.in>
2020-03-28 20:37:05 +0530

    Initializing the getter and setter for both the variables.

--- src/problem3/Student/Student.java
+++ src/problem3/Student/Student.java
@@ -6,4 +6,36 @@ public class Student {
 
     private int rollNo;
 
+    public Student(String s, int r) {
+
+        name = s;
+
+        rollNo = r;
+
+    }
+
+    public String getName() {
+
+        return name;
+
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+
+    }
+
+    public int getRollNo() {
+
+        return rollNo;
+
+    }
+
+    public void setRollNo(int rollNo) {
+
+        this.rollNo = rollNo;
+
+    }
+
 }

d40c307d0dd6caa391e6a23fb9fb44deef6d503d
aman-gulwani <aman.gulwani_cs18@gla.ac.in>
2020-03-28 18:39:56 +0530

    creating object of Scanner class for taking the details of the student from user.

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -5,10 +5,16 @@
  *  Time: 7:06 PM
  */
 package problem5.main;
+
+import java.util.Scanner;
+
 //executable class
 public class MyMain {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
 
     }
 }
